b0ebb0001291e4f65c3f684aa3575fb779c95da7
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2014-06-07 16:19:00 +0900

    add setting FactoryGirl and DatabaseRewinder.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,9 +34,24 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.before :each do
+    if Capybara.current_driver == :selenium
+      DatabaseRewinder.strategy = :truncation
+    else
+      DatabaseRewinder.strategy = :transaction
+    end
+    DatabaseRewinder.start
+  end
+
+  config.after(:each) do
+    DatabaseRewinder.clean 
+  end
+
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.include FactoryGirl::Syntax::Methods
 end
